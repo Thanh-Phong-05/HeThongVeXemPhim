6c42d8c1cea68aec61bfef6cc1ad653513b32174
Thanh Phong <phongpronood@gmail.com>
2025-08-21 13:01:17 +0700

    Menu

--- CinemaClient/Program.cs
+++ CinemaClient/Program.cs
@@ -3,7 +3,16 @@ using System.Text;
 using System.Text.Json;
 
 
-namespace CinemaClient
-{
-    
-}
+namespace CinemaClient;
+
+    Console.WriteLine(await reader.ReadLineAsync());
+
+    while (true)
+    {
+        Console.WriteLine("\nChọn thao tác: 1) List Movies 2) List Shows 3) View Seats 4) Book 5) Release 0) Exit");
+        Console.Write("> ");
+        var key = Console.ReadLine()?.Trim();
+        if (key == "0" || key?.ToLower() == "exit") break;
+    }
+
+
