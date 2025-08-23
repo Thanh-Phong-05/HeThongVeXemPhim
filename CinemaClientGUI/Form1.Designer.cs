674b3eb69febf178d7c4d4a6cf1db95c17c48a7e
Thanh Phong <phongpronood@gmail.com>
2025-08-24 01:01:16 +0700

    Các nút tương tác UI

--- CinemaClientGUI/Form1.Designer.cs
+++ CinemaClientGUI/Form1.Designer.cs
@@ -1,38 +1,22 @@
-﻿namespace CinemaClientGUI;
-
-partial class Form1
+﻿namespace CinemaClientGUI
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
-        {
-            components.Dispose();
-        }
-        base.Dispose(disposing);
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox txtMovieId;
+        private System.Windows.Forms.TextBox txtShowId;
+        private System.Windows.Forms.TextBox txtSeats;
+        private System.Windows.Forms.TextBox txtOutput;
+
+        private System.Windows.Forms.Button btnListMovies;
+        private System.Windows.Forms.Button btnListShows;
+        private System.Windows.Forms.Button btnViewSeats;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Button btnRelease;
+
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
-
-    #endregion
 }
